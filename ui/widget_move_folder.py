0f49e025d126044ad14ee03d4a605c4a685733eb
PPJUST <106465946+ppjust@users.noreply.github.com>
2024-03-17 11:00:07 +0800

    优化pynput的数字小键盘bug的解决方案

--- ui/widget_move_folder.py
+++ ui/widget_move_folder.py
@@ -4,10 +4,10 @@ import os.path
 from PySide6.QtCore import Signal
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import *
-from module import pynput_fix_hotkey
 
 from constant import ICON_ASK_FOLDER, ICON_MOVE, ICON_OPEN_FOLDER, ICON_CLEAR
 from module import function_config
+from module import pynput_fix_hotkey
 from ui.lineedit_drop_path import LineEditDropPath
 from ui.ui_widget_move_folder import Ui_Frame
 
@@ -100,7 +100,7 @@ class WidgetMoveFolder(QFrame):
         """绑定快捷键"""
         if self._hotkey_thread:  # 先停止再绑定
             self._hotkey_thread.stop()
-        self._hotkey_thread = pynput_fix_hotkey.GlobalHotKeys({
+        self._hotkey_thread = pynput_fix_hotkey.GlobalHotKeysFix({
             hotkey: self.do_move, })
 
     def enable_hotkey(self):
